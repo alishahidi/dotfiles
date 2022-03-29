03f0968ea7eedbabcb7678f09ade664e569fecee
Ali Shahidi <alishahidi1376@gmail.com>
2022-03-30 02:52:20 +0430

    fix brightnes on some device

--- installation/Arch/packages/i3wm.sh
+++ installation/Arch/packages/i3wm.sh
@@ -1,1 +1,1 @@
-sudo pacman -S xorg rxvt-unicode picom nitrogen blueberry xfce4-clipman-plugin  lxappearance dmenu nautilus arandr rofi dunst pacman-contrib i3-wm i3lock i3status feh acpilight picom i3-gaps xautolock scrot 
+sudo pacman -S xorg rxvt-unicode picom nitrogen blueberry xfce4-clipman-plugin  lxappearance dmenu nautilus arandr rofi dunst pacman-contrib i3-wm i3lock i3status feh acpilight picom i3-gaps xautolock scrot brightnessctl 
